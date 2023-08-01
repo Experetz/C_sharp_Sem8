ab86c91e092856199f5c9ce673578be2d7a03d0a
Expertz <xeniapliner@gmail.com>
2023-08-01 11:32:56 +0300

    56 Task

---
+++ Sem8_HW_54Task_SortColumn_2x_array/Program.cs
@@ -0,0 +1,54 @@
+﻿/* Задача 54: Задайте двумерный массив. Напишите программу, которая 
+упорядочит по убыванию элементы каждой строки двумерного массива.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+В итоге получается вот такой массив:
+7 4 2 1
+9 5 3 2
+8 4 4 2 */
+
+void SortRowsMaxToMin(int[,] array)
+    {
+        int rows = array.GetLength(0);
+        int columns = array.GetLength(1);
+
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < columns - 1; j++)
+            {
+                for (int k = 0; k < columns - j - 1; k++)
+                {
+                    if (array[i, k] < array[i, k + 1])
+                    {
+                        int temp = array[i, k];
+                        array[i, k] = array[i, k + 1];
+                        array[i, k + 1] = temp;
+                    }
+                }
+            }
+        }
+    }
+
+void PrintArray(int[,] array)
+    {
+        int rows = array.GetLength(0);
+        int columns = array.GetLength(1);
+
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < columns; j++)
+            {
+                Console.Write(array[i, j] + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+
+ int[,] array = new int[,] { { 1, 4, 7, 2 }, { 5, 9, 2, 3 }, { 8, 4, 2, 4 } };
+   Console.WriteLine("\n Initial array is  \n");
+  PrintArray(array);
+  SortRowsMaxToMin(array);
+  Console.WriteLine("\n Sorted array is  \n");
+  PrintArray(array);
---
+++ Sem8_HW_56Task_MinSum_Str/Program.cs
@@ -0,0 +1,58 @@
+﻿/*Задача 56: Задайте прямоугольный двумерный массив. 
+Напишите программу, которая будет находить строку 
+с наименьшей суммой элементов.
+
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+5 2 6 7
+Программа считает сумму элементов в каждой строке и выдаёт 
+номер строки с наименьшей суммой элементов: 1 строка */
+
+void PrintArray(int[,] array)
+    {
+        int rows = array.GetLength(0);
+        int columns = array.GetLength(1);
+
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < columns; j++)
+            {
+                Console.Write(array[i, j] + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+
+
+int FindMinSumRow(int[,] array)
+{
+    int rows = array.GetLength(0);
+    int columns = array.GetLength(1);
+
+    int minSum = int.MaxValue;
+    int rowWithMinSum = -1;
+    
+    for (int i = 0; i < rows; i++)
+    {
+        int sum = 0; 
+         for (int j = 0; j < columns; j++)
+         {
+            sum+= array[i,j];
+         }
+         if(sum<minSum) 
+         {
+            minSum = sum;
+            rowWithMinSum = i;
+         }   
+    }
+    return rowWithMinSum;
+}
+
+
+int[,] array = new int[,] {{1, 4, 7, 2}, {5, 9, 2, 3,}, {8, 4, 2, 4}, {5, 2, 6, 7}};
+Console.WriteLine("\n Your array is  \n");
+PrintArray(array);
+int rowWithMinSum = FindMinSumRow(array);
+Console.WriteLine($"Row with the minimum elements sum is: {rowWithMinSum+1}");
